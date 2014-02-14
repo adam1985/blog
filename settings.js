d136207cde9de75172e4fb5a4aed8f8941056417
pangyuanxing <308106832@qq.com>
2014-02-14 14:30:30 +0800

    upload to heroku

---
+++ settings.js
@@ -0,0 +1,10 @@
+/**
+ * 用于保存该博客工程的配置信息
+ * @type {{cookieSecret: string, db: string, host: string}}
+ */
+
+module.exports = {
+    cookieSecret: 'myblog',
+    db: 'blog',
+    host: 'localhost'
+}; 
